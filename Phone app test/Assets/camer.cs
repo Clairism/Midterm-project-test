d17e9a800c687b6a6deb1703588fadcebbb074c9
Claire Hu <hum085@newschool.edu>
2016-02-26 19:32:19 -0500

    mac cameraTexture working.

---
+++ Phone app test/Assets/camer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camer : MonoBehaviour {
+
+	WebCamTexture front;
+
+	void Start () {
+
+		front = new WebCamTexture ();
+
+		GetComponent<Renderer> ().material.mainTexture = front;
+		front.Play ();
+
+		if (front.isPlaying){
+			GetComponent<Renderer> ().material.mainTexture = front;
+			}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
