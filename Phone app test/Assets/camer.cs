c46fee2085fd63047793fbb40f54cf00a934873b
Claire Hu <hum085@newschool.edu>
2016-02-27 00:47:20 -0500

    saveImg working

--- Phone app test/Assets/camer.cs
+++ Phone app test/Assets/camer.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class camer : MonoBehaviour {
 
 	WebCamTexture front;
+	WebCamTexture saveImg;
+
+	//float time;
+	float timeLimit;
+	int photoNum;
 
 	void Start () {
 
@@ -15,11 +21,51 @@ public class camer : MonoBehaviour {
 		if (front.isPlaying){
 			GetComponent<Renderer> ().material.mainTexture = front;
 			}
+
+		//time = 0.0f;
+		timeLimit = 0.02f;
 	
+		photoNum = 0;
+	
+		print(Application.persistentDataPath);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Time.deltaTime > timeLimit) {
+			//Debug.Log (Time.deltaTime);
+			
+			saveImg = front;
+
+			TakePhoto ();
+			photoNum += 1;
+
+			//time = 0.0f;
+		}
 	
 	}
+
+	void TakePhoto()
+	{
+
+		Texture2D photo = new Texture2D(saveImg.width, saveImg.height);
+		photo.SetPixels(saveImg.GetPixels());
+		photo.Apply();
+
+		//Encode to a PNG
+		byte[] bytes = photo.EncodeToPNG();
+		string path = "/Users/Nighttac";
+		string photoName = "photo " + photoNum + ".png";
+
+		//Write out the PNG
+		System.IO.File.WriteAllBytes (Application.persistentDataPath + photoName, bytes);
+		//File.Move (file, macPath);
+
+		Debug.Log ("photo" + photoNum + "is taken");
+
+
+	}
+
 }
